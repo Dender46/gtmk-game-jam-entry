976062c98709db936a0a08065552aef971205887
Dmitry <dmitriy.bagandov@gmail.com>
2020-07-11 23:30:35 +0300

    Cleanup

--- Assets/Scripts/DrinkProps.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DrinkProps : MonoBehaviour
-{
-    public float alcoholPower;
-    public Vector3 alcoholForce;
-}
