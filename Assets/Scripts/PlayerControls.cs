28662e5595e5cb6be35c09dbfb658e373fb9cabd
Dmitry <dmitriy.bagandov@gmail.com>
2020-07-12 17:52:56 +0300

    Add animtions to player and drinking constraints to player

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,23 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour {
 
 	CharacterController2D movement;
 	PlayerInputActions playerActions;
+	Animator animator;
 	[Range(0.0f, 5.0f)] [SerializeField] float colliderRadius = 0.5f;
 
 	public float speed = 130f;
 
 	float horizontalMove;
 	bool jump	= false;
+	bool canMove = true;
+	bool canDrink = true;
 
 	void Awake() {
 		movement = GetComponent<CharacterController2D>();
 		playerActions = new PlayerInputActions();
+		animator = transform.Find("Texture").GetComponent<Animator>();
+
 		playerActions.Player.Move.performed += ctx => horizontalMove = ctx.ReadValue<Vector2>().x;
 		playerActions.Player.Jump.performed += ctx => jump = true;
-		playerActions.Player.Use.performed += ctx => Use();
+		playerActions.Player.Use.performed += ctx => StartCoroutine(Use());
 	}
 	
 	void OnDrawGizmos()
@@ -27,15 +33,33 @@ public class PlayerControls : MonoBehaviour {
         Gizmos.DrawSphere(transform.position, colliderRadius);
     }
 
-	void Use() {
+	IEnumerator Use() {
+		if (!canDrink)
+			yield break;
+
 		Collider2D hitCollider = Physics2D.OverlapCircle(transform.position, colliderRadius, LayerMask.GetMask("Drinks"), -0.5f, 0.5f);
 		if (hitCollider) {
 			DrinkScript script = hitCollider.gameObject.GetComponent<DrinkScript>();
 			script.Use();
+			
+			canMove = false;
+			canDrink = false;
+			animator.SetBool("Drinking", true);
+			
+			yield return new WaitForSeconds(1.0f);
+			
+			canMove = true;
+			canDrink = true;
+			animator.SetBool("Drinking", false);
 		}
 	}
 
 	void FixedUpdate() {
+		if (!canMove) {
+			movement.Move (0.0f, false, jump);
+			return;
+		}
+
 		movement.Move (horizontalMove * speed * Time.fixedDeltaTime, false, jump);
 		jump = false;
 	}
