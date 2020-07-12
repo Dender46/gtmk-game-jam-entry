ae990c3668780cd7d5afc765537f819cb4bae719
Dmitry <dmitriy.bagandov@gmail.com>
2020-07-12 20:50:16 +0300

    Drunk rotation

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,14 +1,21 @@
-﻿using UnityEngine;
+﻿	using UnityEngine;
+using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
 	private Transform target;
 	private Vector3 velocity = Vector3.zero;
+	private Vector3 rvelocity = Vector3.zero;
 	private GameData gameData;
 
 	public Vector3 offset;
 	public float smoothSpeed = 0.6f;
+	
 	public float drunkSwayness;
+	public float drunkOffsetUpdate = 1.0f;
+
+	private Vector3 drunkOffset = Vector3.zero;
+	private Vector3 drunkRotation = Vector3.zero;
 
 	private void Start() {
 		target = GameObject.Find("Player").transform;
@@ -16,15 +23,28 @@ public class CameraFollow : MonoBehaviour {
 
 		// Fixes jittery movement of player
 		GameObject.Find("Player").GetComponent<Rigidbody2D>().interpolation = RigidbodyInterpolation2D.Interpolate;
+		StartCoroutine(UpdateDrunkOffset());
 	}
 
 	void Update () {
+		Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, target.position + offset + drunkOffset, ref velocity, smoothSpeed);
+		transform.position = smoothedPosition;
+
+		Vector3 smoothedRotation = Vector3.SmoothDamp(transform.eulerAngles, transform.eulerAngles + drunkRotation, ref rvelocity, smoothSpeed);
+		transform.eulerAngles = smoothedRotation;
+	}
+
+	IEnumerator UpdateDrunkOffset() {
+		yield return new WaitForSeconds(drunkOffsetUpdate);
+
 		float drunkLevel = gameData.playerAlcoholLevel * drunkSwayness;
 		float randomX = Random.Range(-drunkLevel, drunkLevel);
 		float randomY = Random.Range(-drunkLevel, drunkLevel);
 		float randomZ = Random.Range(-drunkLevel, drunkLevel);
-		Vector3 drunkOffset = new Vector3(randomX, randomY, randomZ);
-		Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, target.position + offset + drunkOffset, ref velocity, smoothSpeed);
-		transform.position = smoothedPosition;
+		drunkOffset = new Vector3(randomX, randomY, randomZ);
+
+		drunkRotation = new Vector3(0.0f, 0.0f, Random.Range(-drunkLevel, drunkLevel));
+
+		StartCoroutine(UpdateDrunkOffset());
 	}
 }
